546b888cc487841bcb35ec7f6a2ce6d515554005
quiuzumaki <qui.dobk1002cla@hcmut.edu.vn>
2024-04-11 02:48:06 +0700

    Scan memory of buffer

---
+++ YaraScanner.py
@@ -0,0 +1,18 @@
+import yara
+
+class Yara:
+    def __init__(self) -> None:
+        self.rules = yara.compile('./rules/rules_file.yar')
+        self.matches = None
+
+    def scan_memory(self, data: bytes) -> bool:
+        if not isinstance(data, bytes): return False
+        self.matches = self.rules.match(data=data)
+        return True if len(self.matches) != 0 else False
+
+    def get_matches(self) -> list:
+        return self.matches
+    
+    def get_meta(self):
+        return self.matches[0].meta
+    
