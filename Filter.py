f82746081a08c0dd69bd66f622a067db1440ce50
quiuzumaki <qui.dobk1002cla@hcmut.edu.vn>
2024-04-11 02:48:47 +0700

    Filter the path of files/registry were created/opened

---
+++ Filter.py
@@ -0,0 +1,19 @@
+system_paths = [
+    "C:\\Program Files".lower(),
+    "C:\\Program Files (x86)".lower(),
+    "C:\\ProgramData".lower(),
+    "C:\\Windows".lower()
+]
+
+filenames = [
+    "*",
+    "ConsoleHost_history.txt".lower(),
+    "StartupProfileData-Interactive".lower()
+]
+
+persistence_path = [
+    'Software\\Microsoft\\Windows\\CurrentVersion\\Run'.lower(),
+    'Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce'.lower(),
+    'Software\\Microsoft\\Windows\\CurrentVersion\\RunOnceEx'.lower(),
+    'Software\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon'.lower()
+]
